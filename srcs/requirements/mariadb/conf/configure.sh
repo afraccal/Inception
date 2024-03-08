a242e6ebb8ad4d633670e2616fd9b9773e3e6d9d
Alessandro Fraccalvieri <alessandrofraccalvieri@MacBook-Pro-di-Alessandro.local>
2024-03-08 17:27:37 +0100

    cambia tuttooooo

--- srcs/requirements/mariadb/conf/configure.sh
+++ srcs/requirements/mariadb/conf/configure.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
 service mariadb start
-mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 sleep 0.5
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -uroot -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
 sleep 0.5
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mariadb -uroot -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 sleep 0.5
-mysql -e "FLUSH PRIVILEGES;"
+mariadb -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+sleep 0.5
+mariadb -uroot -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 sleep 0.5
 mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
