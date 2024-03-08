0a7ed8a45d490f9fedfda18bf197fc7cbf263ac9
Alessandro Fraccalvieri <alessandrofraccalvieri@MacBook-Pro-di-Alessandro.local>
2024-03-08 16:23:34 +0100

    cambia tuttooooo

--- srcs/requirements/mariadb/conf/configure.sh
+++ srcs/requirements/mariadb/conf/configure.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 service mariadb start
-mysql -e "CREATE DATABASE IF NOT EXISTS $DB_HOST;"
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 sleep 0.5
-mysql -e "CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
 sleep 0.5
-mysql -e "GRANT ALL PRIVILEGES ON $DB_HOST.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
 sleep 0.5
 mysql -e "FLUSH PRIVILEGES;"
 sleep 0.5
-mysqladmin -u root -p$MYSQL_ROOT_PWD shutdown
+mysqladmin -u root -p$MYSQL_ROOT_PASSWORD shutdown
 exec mysqld_safe
--- srcs/requirements/wordpress/conf/configure.sh
+++ srcs/requirements/wordpress/conf/configure.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
 cd /var/www/html/wordpress
 chmod +x wp-cli.phar
-./wp-cli.phar config create --path=/var/www/html/wordpress --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST:3306 --allow-root
+./wp-cli.phar config create --path=/var/www/html/wordpress --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --allow-root./wp-cli.phar core install --url=afraccal.42.fr --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
 ./wp-cli.phar core install --url=afraccal.42.fr --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
 ./wp-cli.phar user create giorgio giorgio@gmail.com --role=author --porcelain --allow-root
 chown -R www-data:www-data /var/www/html/wordpress/
