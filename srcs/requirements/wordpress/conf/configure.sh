e78a909ddaf940e37fd2b34e255f72c931be7897
Alessandro Fraccalvieri <alessandrofraccalvieri@MacBook-Pro-di-Alessandro.local>
2024-03-08 15:45:10 +0100

    wordpress

--- srcs/requirements/wordpress/conf/configure.sh
+++ srcs/requirements/wordpress/conf/configure.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 
-if [ -f /tmp/switch.tmp ]; then
-	# Set Env
-	sed -i -e "s/\${DB_NAME}/${DB_NAME}/g" /var/www/wordpress/wp-config.php;
-	sed -i -e "s/\${DB_USER}/${DB_USER}/g" /var/www/wordpress/wp-config.php;
-	sed -i -e "s/\${DB_PASSWORD}/${DB_PASSWORD}/g" /var/www/wordpress/wp-config.php;
-	sed -i -e "s/\${DB_HOST}/${DB_HOST}/g" /var/www/wordpress/wp-config.php;
-
-	# Create the php folder
-	mkdir -p ./run/php/;
-
-	# Remove the switch
-	rm -f /tmp/switch.tmp;
-
-fi
-
-# Entrypoint
-/usr/sbin/php-fpm7.3 -F;
+cd /var/www/html/wordpress
+chmod +x wp-cli.phar
+./wp-cli.phar config create --path=/var/www/html/wordpress --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST:3306 --allow-root
+./wp-cli.phar core install --url=afraccal.42.fr --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
+./wp-cli.phar user create giorgio giorgio@gmail.com --role=author --porcelain --allow-root
+chown -R www-data:www-data /var/www/html/wordpress/
+/usr/sbin/php-fpm8.2 -F;
