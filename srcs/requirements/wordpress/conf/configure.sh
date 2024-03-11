886878c9a7cba4b942ca1395fde698d970597650
Alessandro Fraccalvieri <alessandrofraccalvieri@MacBook-Pro-di-Alessandro.local>
2024-03-11 11:16:46 +0100

    image and conf word

--- srcs/requirements/wordpress/conf/configure.sh
+++ srcs/requirements/wordpress/conf/configure.sh
@@ -1,7 +1,7 @@
-cd /var/www/html/wordpress
+cd /var/www/html
 chmod +x wp-cli.phar
 ./wp-cli.phar config create --path=/var/www/html/wordpress --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb:3306 --allow-root
 ./wp-cli.phar core install --url=$DOMAIN_NAME/ --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --skip-email --allow-root
 ./wp-cli.phar user create ale ale42@gmail.com --role=author --porcelain --allow-root
-chown -R www-data:www-data /var/www/html/wordpress/
+chown -R www-data:www-data /var/www/html
 /usr/sbin/php-fpm8.2 -F
